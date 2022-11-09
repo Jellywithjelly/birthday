76076093e1d945751a996c250d096de3d0ccb7a7
Shape of My Heart <61645744+Jellywithjelly@users.noreply.github.com>
2022-11-09 10:45:40 +0800

    Add files via upload

---
+++ HappyBirthday_files/happy.js
@@ -0,0 +1,35 @@
+$(function () {
+
+
+    $("#start").click(function(){
+        $('audio')[0].play();
+    })
+
+    setTimeout(function () {
+        $('.name').animate({
+            opacity:"1",
+            top:"15%"
+        },2000);
+    },6000);
+    setTimeout(function () {
+        $('.happy').animate({
+            opacity:"1",
+            top:"15%"
+        },2000);
+
+    },6000);
+    setTimeout(function () {
+        $('.button-style1').animate({
+            opacity:"1",
+            top:"70%"
+        },1500);
+        $('.button-style2').animate({
+            opacity:"1",
+            top:"85%"
+        },2000);
+    },9000);
+	$(window).click(function(){
+		$('audio')[0].play();
+	});
+    
+})
